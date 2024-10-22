633584744d91140759afbf3596ab7e36b160b5ea
imgavinhi <gavinhunsinger11@gmail.com>
2024-10-21 23:02:06 -0400

    Adding definitions to statistics.py

--- Telemetry_Data_Gatherer/statistics.py
+++ Telemetry_Data_Gatherer/statistics.py
@@ -27,8 +27,11 @@ def statistics(csv_file):
             if packet_count > 0:
                 av_size /= packet_count
         
-        print(packet_count)
-        print(av_size)
-        print(max_size)
-        print(min_size)
+    print("Definitions: ")
+    print("|Correlation|\nA statistical measurement that shows the extent that two variables are linearly related. This means that the two variables change together at a constant rate. It’s a tool used for describing simple relationships without making a statement about causes and effects.\n|Correlation Coefficient|\n Correlation coefficient is a number between –1 and+1 calculated to represent the linear relationship of two variables. Negative one represents a perfect negative correlation, meaning when one variable increases the other decreases proportionally. Positive 1 indicates that two variables have a perfect positive correlation, meaning when one increases the other increases proportionally.\n|Correlation Metrics|\nCorrelation metrics measure whether or not there is a relationship between two variables. Some common ones include correlation coefficients, matrices, and covariance.\n|Correlation Matrix|\nA correlation matrix is a table that shows the correlation coefficients between a set of variables. A correlation matrix can be used to identify patterns and trends in data as well as to understand the relationships between multiple variables.")
+
+    print("Count: " + str(packet_count))
+    print("Average Packet Length: " + str(av_size))
+    print("Maximum Packet Length: " + str(max_size))
+    print("Minimum Packet Length: " + str(min_size))
                 #packet_type = row[7] #PID vs EtherType?
