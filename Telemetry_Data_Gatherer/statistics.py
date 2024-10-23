b39b0e732b8ea93e644c872e49c1698f950fc0fa
imgavinhi <gavinhunsinger11@gmail.com>
2024-10-23 10:28:46 -0400

    Packet distribution works now it just does not show percentages for distribution yet

--- Telemetry_Data_Gatherer/statistics.py
+++ Telemetry_Data_Gatherer/statistics.py
@@ -28,7 +28,16 @@ def statistics(csv_file):
 
             if packet_count > 0:
                 av_size /= packet_count
-        
+
+            if row[3] == "0806":
+                packet_dictionary['ARP'] += 1
+            if row[3] == "0800":
+                if row[7] == "01":
+                    packet_dictionary['ICMP'] += 1
+                elif row[7] == "06":
+                    packet_dictionary['TCP'] += 1
+                elif row[7] == "11":
+                    packet_dictionary['UDP'] += 1        
     print("Definitions: ")
     print("|Correlation|\nA statistical measurement that shows the extent that two variables are linearly related. This means that the two variables change together at a constant rate. It’s a tool used for describing simple relationships without making a statement about causes and effects.\n|Correlation Coefficient|\n Correlation coefficient is a number between –1 and+1 calculated to represent the linear relationship of two variables. Negative one represents a perfect negative correlation, meaning when one variable increases the other decreases proportionally. Positive 1 indicates that two variables have a perfect positive correlation, meaning when one increases the other increases proportionally.\n|Correlation Metrics|\nCorrelation metrics measure whether or not there is a relationship between two variables. Some common ones include correlation coefficients, matrices, and covariance.\n|Correlation Matrix|\nA correlation matrix is a table that shows the correlation coefficients between a set of variables. A correlation matrix can be used to identify patterns and trends in data as well as to understand the relationships between multiple variables.")
 
@@ -40,15 +49,6 @@ def statistics(csv_file):
     #PID is used for any packet classification in an IPv4 packet (or IPv6 but don't worry about that for now)
     #ethertype is used to distinguish IPv4, IPv6, or ARP
     #packet_type = row[7]
-    if row[3] == "0806":
-        packet_dictionary['ARP'] += 1
-    if row[3] == "0800":
-        if row[7] == "01":
-            packet_dictionary['ICMP'] += 1
-        elif row[7] == "06":
-            packet_dictionary['TCP'] += 1
-        elif row[7] == "11":
-            packet_dictionary['UDP'] += 1
 
     print(packet_dictionary)
 
