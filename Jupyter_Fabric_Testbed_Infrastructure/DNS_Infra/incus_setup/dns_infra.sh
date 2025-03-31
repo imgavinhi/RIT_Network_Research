1d34e5c6ea90b59643026f86640f13f49463fca4
Gavin Hunsinger <gavinhunsinger11@gmail.com>
2025-03-31 14:42:20 -0400

    updating to make fabric topology

--- Jupyter_Fabric_Testbed_Infrastructure/DNS_Infra/incus_setup/dns_infra.sh
+++ Jupyter_Fabric_Testbed_Infrastructure/DNS_Infra/incus_setup/dns_infra.sh
@@ -10,8 +10,8 @@ incus network rm dns-infra 2>/dev/null || true
 
 # Creates the network and containers
 incus network create dns-infra network=UPLINK ipv4.address=192.168.47.1/24 ipv4.nat=true ipv6.address=none ipv6.nat=false
-incus init images:ubuntu/jammy/cloud ntp-make -t c2-m6 --network dns-infra -d eth0,ipv4.address=192.168.47.50 -d root,size=320GiB
-incus init images:ubuntu/jammy/cloud ntp-client -t c2-m6 --network dns-infra -d eth0,ipv4.address=192.168.47.101 -d root,size=320GiB
+incus init images:ubuntu/jammy/cloud ntp-client -t c2-m6 --network dns-infra -d eth0,ipv4.address=192.168.47.50 -d root,size=320GiB
+incus init images:ubuntu/jammy/cloud ntp-make -t c2-m6 --network dns-infra -d eth0,ipv4.address=192.168.47.101 -d root,size=320GiB
 
 # Starts the containers
 echo "========== Starting VMs =========="
@@ -21,48 +21,48 @@ incus start ntp-client
 # Wait for network to initialize
 sleep 5
 
-# Setup ntp-client (DNS Server)
+# Setup ntp-make (DNS Server)
 echo "========== Setting up ntp-client (DNS Server) =========="
-incus exec ntp-client -- /bin/bash -c "apt update && sleep 5 && DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools bind9 bind9utils bind9-doc dnsutils tshark"
+incus exec ntp-make -- /bin/bash -c "apt update && sleep 5 && DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools bind9 bind9utils bind9-doc dnsutils tshark"
 
 # Ensure bind9 directories exist
-incus exec ntp-client -- /bin/bash -c "mkdir -p /etc/bind/zones"
+incus exec ntp-make -- /bin/bash -c "mkdir -p /etc/bind/zones"
 
 # Enable and start bind9
-incus exec ntp-client -- systemctl enable --now bind9
+incus exec ntp-make -- systemctl enable --now bind9
 
-# Setup ntp-make (DNS Client)
+# Setup ntp-client (DNS Client)
 echo "========== Setting up ntp-make (DNS Client) =========="
-incus exec ntp-make -- /bin/bash -c "apt update && sleep 5"
-incus exec ntp-make -- bash -c "useradd -m -s /bin/bash 'ansible'"
-incus exec ntp-make -- usermod -aG sudo ansible
-incus exec ntp-make -- bash -c "echo ansible:ansible | chpasswd"
+incus exec ntp-client -- /bin/bash -c "apt update && sleep 5"
+incus exec ntp-client -- bash -c "useradd -m -s /bin/bash 'ansible'"
+incus exec ntp-client -- usermod -aG sudo ansible
+incus exec ntp-client -- bash -c "echo ansible:ansible | chpasswd"
 
 # Install nslookup (dnsutils) and tshark
-incus exec ntp-make -- /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y dnsutils tshark net-tools"
+incus exec ntp-client -- /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y dnsutils tshark net-tools"
 
 # Allow ansible user to run tshark without root privileges
-incus exec ntp-make -- /bin/bash -c "usermod -aG wireshark ansible"
+incus exec ntp-client -- /bin/bash -c "usermod -aG wireshark ansible"
 
 # Push DNS configuration files
-echo "========== Configuring DNS on ntp-client =========="
-incus file push ../named.conf.options ntp-client/etc/bind/named.conf.options
-incus file push ../named.conf.local ntp-client/etc/bind/named.conf.local
-incus file push ../research.net ntp-client/etc/bind/zones/research.net
-incus file push ../research.net.rev ntp-client/etc/bind/zones/research.net.rev
-incus file push ../resolv.conf ntp-client/etc/resolv.conf
-
-# Restart bind9 to apply changes
-incus exec ntp-client -- systemctl restart bind9
-incus exec ntp-client -- systemctl status bind9 --no-pager
-
-# Set resolv.conf on ntp-make
-echo "========== Applying resolv.conf to ntp-make =========="
+echo "========== Configuring DNS on ntp-make =========="
+incus file push ../named.conf.options ntp-make/etc/bind/named.conf.options
+incus file push ../named.conf.local ntp-make/etc/bind/named.conf.local
+incus file push ../research.net ntp-make/etc/bind/zones/research.net
+incus file push ../research.net.rev ntp-make/etc/bind/zones/research.net.rev
 incus file push ../resolv.conf ntp-make/etc/resolv.conf
 
+# Restart bind9 to apply changes
+incus exec ntp-make -- systemctl restart bind9
+incus exec ntp-make -- systemctl status bind9 --no-pager
+
+# Set resolv.conf on ntp-client
+echo "========== Applying resolv.conf to ntp-make =========="
+incus file push ../resolv.conf ntp-client/etc/resolv.conf
+
 # Add login messages
-incus exec ntp-client -- /bin/bash -c 'echo "reset; echo YOU ARE LOGGED IN AS ROOT IN ntp-client" >> /root/.bashrc'
 incus exec ntp-make -- /bin/bash -c 'echo "reset; echo YOU ARE LOGGED IN AS ROOT IN ntp-make" >> /root/.bashrc'
+incus exec ntp-client -- /bin/bash -c 'echo "reset; echo YOU ARE LOGGED IN AS ROOT IN ntp-client" >> /root/.bashrc'
 
 echo "========== Setup complete! =========="
 
