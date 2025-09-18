50f2d8b6304d23d74edde84169ea3ae85e1983d6
Gavin Hunsinger <gavinhunsinger11@gmail.com>
2025-09-17 20:55:45 -0400

    changing default value to 0. Next step get appropriate data set to try

--- CNN/packet_labeler.py
+++ CNN/packet_labeler.py
@@ -7,6 +7,9 @@ utilizes traff ints of 7 (dns) and 8 (quic) - UDP header with other identifiers
 '''
 def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, icmp_reply_ctr, http_ctr, tls_ctr, dns_ctr, quic_ctr):
     packet = "No Match"
+    # Assign a default value for traffic_class_int
+    traffic_class_int = 0
+    
     l2_type = x_line_data[24:28]
 
     if l2_type == '0806':
@@ -14,18 +17,18 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
         #print("ARP:", arp_type_code)
         if arp_type_code == "0001":
             packet = "ARP Echo Request"
-            traffic_class_int = 0
+            traffic_class_int = 1
             arp_request_ctr = arp_request_ctr + 1
         elif arp_type_code == "0002":
             packet = "ARP Echo Reply"
-            traffic_class_int = 1
+            traffic_class_int = 2
             arp_reply_ctr = arp_reply_ctr + 1
 
     elif l2_type == '0800':
 
         l3_pid = x_line_data[46:48]
         l3_type = x_line_data[24:28] #located in the ethernet header specifies IPv4
-    
+        
         '''
         tesing to make sure ICMP Echo request are properly parsed (they are)
         print(x_line_data)
@@ -47,25 +50,25 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
                 #reply type/code = 0000
                 if icmp_type_code == '0800':
                     packet = 'IPv4 ICMP Request'
-                    traffic_class_int = 2
+                    traffic_class_int = 3
                     icmp_request_ctr = icmp_request_ctr+1
                 elif icmp_type_code == '0000':
                     packet = 'IPv4 ICMP Reply'
-                    traffic_class_int = 3
+                    traffic_class_int = 4
                     icmp_reply_ctr = icmp_reply_ctr+1
             #print("Req: ", icmp_request_ctr, " Rep: ", icmp_reply_ctr) tesing
             #print(traffic_class_int)
 
-            elif l3_pid == '02': #TCP l3 pid
+            elif l3_pid == '06': #TCP l3 pid
                 #http (port 80 = 0050) and tls (port 443 == 01bb)
-                if src_port == "0500" or dst_port == "0050":
+                if src_port == "0050" or dst_port == "0050":
                     packet = "IPv4 TCP HTTP"
-                    traffic_int_class = 6
+                    traffic_class_int = 6
                     #increase http counters when implemented
                     http_ctr += 1
                 elif src_port == "01bb" or dst_port == "01bb":
                     packet = "IPv4 TCP TLS"
-                    traffic_int_class = 5
+                    traffic_class_int = 5
                     tls_ctr += 1
                     #increase tls counters when implemented
 
