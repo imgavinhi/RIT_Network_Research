9100bb259f514ba8d9d185cec9f31fc97ad4883f
Gavin Hunsinger <gavinhunsinger11@gmail.com>
2025-09-25 16:09:45 -0400

    updating to resolve errors

--- CNN/packet_labeler.py
+++ CNN/packet_labeler.py
@@ -1,14 +1,14 @@
 '''
 this function defines labels for ARP and IPv4 packets. So far it only does ICMP Echo Requests and Echo Replies
-utilizes traffic class ints of 1 (arp requests) and 2 (arp replies)
-utilizes traffic class ints of 3 (icmp requests) and 4 (icmp replies)
-utilizes traffic class ints of 5 (tls) and 6 (http) - TCP header with other identifiers
-utilizes traff ints of 7 (dns) and 8 (quic) - UDP header with other identifiers
+utilizes traffic class ints of 0 (arp requests) and 1 (arp replies)
+utilizes traffic class ints of 2 (icmp requests) and 3 (icmp replies)
+utilizes traffic class ints of 4 (tls) and 5 (http) - TCP header with other identifiers
+utilizes traff ints of 6 (dns) and 7 (quic) - UDP header with other identifiers
 '''
 def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, icmp_reply_ctr, http_ctr, tls_ctr, dns_ctr, quic_ctr):
     packet = "No Match"
     # Assign a default value for traffic_class_int
-    traffic_class_int = 0
+    traffic_class_int = -1
     
     l2_type = x_line_data[24:28]
 
@@ -17,11 +17,11 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
         #print("ARP:", arp_type_code)
         if arp_type_code == "0001":
             packet = "ARP Echo Request"
-            traffic_class_int = 1
+            traffic_class_int = 0
             arp_request_ctr = arp_request_ctr + 1
         elif arp_type_code == "0002":
             packet = "ARP Echo Reply"
-            traffic_class_int = 2
+            traffic_class_int = 1
             arp_reply_ctr = arp_reply_ctr + 1
 
     elif l2_type == '0800':
@@ -50,11 +50,11 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
                 #reply type/code = 0000
                 if icmp_type_code == '0800':
                     packet = 'IPv4 ICMP Request'
-                    traffic_class_int = 3
+                    traffic_class_int = 2
                     icmp_request_ctr = icmp_request_ctr+1
                 elif icmp_type_code == '0000':
                     packet = 'IPv4 ICMP Reply'
-                    traffic_class_int = 4
+                    traffic_class_int = 3
                     icmp_reply_ctr = icmp_reply_ctr+1
             #print("Req: ", icmp_request_ctr, " Rep: ", icmp_reply_ctr) tesing
             #print(traffic_class_int)
@@ -63,12 +63,12 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
                 #http (port 80 = 0050) and tls (port 443 == 01bb)
                 if src_port == "0050" or dst_port == "0050":
                     packet = "IPv4 TCP HTTP"
-                    traffic_class_int = 6
+                    traffic_class_int = 5
                     #increase http counters when implemented
                     http_ctr += 1
                 elif src_port == "01bb" or dst_port == "01bb":
                     packet = "IPv4 TCP TLS"
-                    traffic_class_int = 5
+                    traffic_class_int = 4
                     tls_ctr += 1
                     #increase tls counters when implemented
 
@@ -76,12 +76,12 @@ def packet_types(x_line_data, arp_request_ctr, arp_reply_ctr, icmp_request_ctr, 
                 #dns (port 53 = 0035) and quic (ports 80 == 0050 and 443 == 01bb)
                 if src_port == "0035" or dst_port == "0035":
                     packet = "IPv4 UDP DNS"
-                    traffic_class_int = 7
+                    traffic_class_int = 6
                     dns_ctr += 1
                     #increase dns counter once implemented
                 elif src_port == "0050" or src_port == "01bb" or dst_port == "0050" or dst_port == "01bb":
                     packet = "IPv4 UDP QUIC"
-                    traffic_class_int = 8
+                    traffic_class_int = 7
                     quic_ctr += 1
                     #increase quic counter once implemented
 
